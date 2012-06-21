7b1aa974b42667fdd755529955550aa4e997a795
Peter Jones <pjones@pmade.com>
2012-06-21 14:49:41 -0600

    Add basic test for the SimpleTwitter::Search class

---
+++ test/search_test.rb
@@ -0,0 +1,23 @@
+################################################################################
+require('test/unit')
+require('mocha')
+require('simple_twitter')
+
+################################################################################
+class SearchTest < Test::Unit::TestCase
+
+  ##############################################################################
+  def test_search_method
+    # Don't talk with Twitter, use a file instead
+    json_file = File.expand_path('gaga.json', File.dirname(__FILE__))
+    response = stub('response', :body => File.read(json_file))
+    HTTParty.expects(:get).returns(response)
+
+    # Test SimpleTwitter::Search#search
+    searcher = SimpleTwitter::Search.new
+    tweets = searcher.search("doesn't matter")
+
+    assert(tweets.is_a?(Hash))
+    assert(tweets.has_key?('results'))
+  end
+end
